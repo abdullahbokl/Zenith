f4a82dfbac5ede2413485bee48dc2d9a197e3a4f
Eng.Bokl <abdullah.bokl@gmail.com>
2026-02-23 01:21:57 +0200

    Initial Commit

---
+++ Zenith/ZenithApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenithApp.swift
+//  Zenith
+//
+//  Created by Abdullah Elbokl on 23/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ZenithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
